c60db43aede829ddca2b3c286cb9259d1e818d71
Mark Brennan <mbrennan456@icloud.com>
2016-03-27 21:47:48 +0100

    Added segue between the 2 views.

--- RSS/ViewController.swift
+++ RSS/ViewController.swift
@@ -19,6 +19,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // This method is called when a segue occurs
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+    }
 
 
 }
